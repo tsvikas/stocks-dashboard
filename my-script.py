e5e14c3ef4968b08accd94d52f34848a53a3dddc
Tsvika Shapira <tsvikas@gmail.com>
2026-05-01 15:45:36 +0300

    Initial commit

---
+++ my-script.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# /// script
+# requires-python = ">=3.14"
+# dependencies = []
+# ///
+# NOTE: use `#!/usr/bin/env -S uv run --script` if there are dependencies
+
+
+def main() -> None:
+    print("Hello from my-script.py!")
+
+
+if __name__ == "__main__":
+    main()
